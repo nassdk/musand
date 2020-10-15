9f3a587287c9ebe5053c406c8809b5a0a2faa7e9
nassdk <nassdk17@gmail.com>
2020-10-16 01:56:01 +0300

    Added injection for ErrorHandler.

--- app/src/main/java/com/nassdk/musand/di/domainModule.kt
+++ app/src/main/java/com/nassdk/musand/di/domainModule.kt
@@ -1,11 +1,14 @@
 package com.nassdk.musand.di
 
+import com.nassdk.musand.data.base.ErrorHandler
 import com.nassdk.musand.data.repositoryimpl.AuthRepositoryImpl
 import com.nassdk.musand.domain.interactor.AuthInteractor
 import com.nassdk.musand.domain.repository.AuthRepository
 import org.koin.dsl.module
 
 val domainModule = module {
+
+    single { ErrorHandler() }
 
     single<AuthRepository> { AuthRepositoryImpl(get()) }
 
